4e66f804d162ac98ff9e761cdca1870a2c95bdaf
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-24 12:58:34 +0800

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -10,4 +10,4 @@ cp -f "$GITHUB_WORKSPACE/scripts/dts/mt7628an_hilink_hlk-7628n.dts" "target/linu
 
 
 # turboacc
-curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh
+# curl -sSL https://raw.githubusercontent.com/mufeng05/turboacc/main/add_turboacc.sh -o add_turboacc.sh && bash add_turboacc.sh

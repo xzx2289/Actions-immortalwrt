97d13ae88ca12109928af07152dad4cc592c8857
xuxin1955 <73999603+xuxin1955@users.noreply.github.com>
2026-04-25 15:08:57 +0800

    Update diy-part6.sh

--- diy-part6.sh
+++ diy-part6.sh
@@ -10,8 +10,9 @@ mkdir -p target/linux/ramips/image/
 cp -f "$GITHUB_WORKSPACE/scripts/image/mt76x8.mk" "target/linux/ramips/image/mt76x8.mk"
 
 # hlk7628dna board.d
-mkdir -p target/linux/ramips/mt76x8/base-files/etc/board.d/
-cp -f "$GITHUB_WORKSPACE/scripts/image/02_network" target/linux/ramips/mt76x8/base-files/etc/board.d/02_network
+mkdir -p "target/linux/ramips/mt76x8/base-files/etc/board.d/"
+cp -f "$GITHUB_WORKSPACE/scripts/image/02_network" "target/linux/ramips/mt76x8/base-files/etc/board.d/02_network"
+
 
 
 
